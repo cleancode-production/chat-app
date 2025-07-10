1a99ff926dfcb6c473c80020655c938793358b6d
MrMister20257 <norman.a.marin@gmail.com>
2025-07-10 14:04:45 +0200

    Entferne roomId aus messageSchema und füge messageId zu roomSchema hinzu

--- server/models/messageModel.js
+++ server/models/messageModel.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const messageSchema = new mongoose.Schema({
     text: { type: String, required: true },
     senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    roomId: { type: mongoose.Schema.Types.ObjectId, ref: "Room", required: true },
     createdAt: { type: String, required: true }
 });
 

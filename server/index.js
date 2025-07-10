cc124eba0f50d063285c0e4914ac8fb8801f030d
DamianToromanovic <d.toromanovic@web.de>
2025-07-10 10:45:27 +0200

    index js and pakcagejson script for starting server added

--- server/index.js
+++ server/index.js
@@ -1,4 +1,40 @@
 import express from "express";
 import cors from "cors";
+import { Server } from "socket.io";
+import http from "http";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("send-message", (data) => {
+    console.log("Nachricht empfangen:", data);
+    socket.broadcast.emit("receive-message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf http://localhost:${PORT}`);
+});

1b85d6912406e5a5159b33f296d9f727e4996936
MrMister20257 <norman.a.marin@gmail.com>
2025-07-10 14:16:27 +0200

    Füge Funktion zur Datumsformatierung hinzu

---
+++ server/lib/date.js
@@ -0,0 +1,10 @@
+export const date = () => {
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, "0");
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const formattedDate = `${day}.${month}.${year},${hours}:${minutes}`;
+    return formattedDate;
+};

3ab9e0b960686bd6efa87d19e70965b513b27133
MrMister20257 <norman.a.marin@gmail.com>
2025-07-11 13:21:39 +0200

    Aktiviere CORS mit spezifischen Ursprüngen und Anmeldeinformationen in der Serverkonfiguration

--- server/index.js
+++ server/index.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 5000;
 
 db.connect();
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "*"],
+    credentials: true,
+  }));
+  
 app.use(express.json());
 app.use(cookieParser());
 
